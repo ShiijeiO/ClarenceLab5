81aed5e8cee7a4ab118dfa417695d117c9d0278c
ShiijeiO <cjmarzan09@yahoo.com>
2024-02-09 17:55:57 -0500

    Implemented AlertDialog

--- ClarenceLab5/src/main/java/com/example/fragmenttry/ClarenceActivity5.java
+++ ClarenceLab5/src/main/java/com/example/fragmenttry/ClarenceActivity5.java
@@ -1,9 +1,10 @@
 //Clarence Oriola N01573843
 package com.example.fragmenttry;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class ClarenceActivity5 extends AppCompatActivity {
 
@@ -11,5 +12,31 @@ public class ClarenceActivity5 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Build the AlertDialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        //AlertDialog requirements
+        builder.setIcon(android.R.drawable.btn_star);
+        builder.setTitle(R.string.project_name);
+        builder.setMessage(R.string.onBackPressed_message);
+
+        //yes button condition
+        builder.setPositiveButton(R.string.onBackPressed_yes, (dialog, which) -> {
+            finish();
+        });
+
+        // no button condition
+        builder.setNegativeButton(R.string.onBackPressed_no, (dialog, which) -> {
+            dialog.dismiss(); //dismisses alertDialog
+        });
+
+        builder.setCancelable(false); //user cannot dismiss
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
